296fe4c2c6806189f5db16bfc3334985dac25cd2
umutcanbas <umutbas57@gmail.com>
2024-12-25 20:11:01 +0300

    Add userId management to Redux slice; implement setUserId reducer and update storage logic

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,31 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit';
 
-import { MMKV } from 'react-native-mmkv'
+import {MMKV} from 'react-native-mmkv';
 
-const storage = new MMKV()
-const isLogged = storage.getBoolean('isLogged')
+const storage = new MMKV();
+const isLogged = storage.getBoolean('isLogged');
+
+const userId = storage.getString('userId');
 
 const initialState = {
-  isLogged: isLogged || false
-}
+  isLogged: isLogged || false,
+  userId: userId || '',
+};
 
 const slice = createSlice({
   name: 'slice',
   initialState,
   reducers: {
     login: state => {
-      state.isLogged = true
-
-      storage.set('isLogged', state.isLogged)
+      state.isLogged = true;
+      storage.set('isLogged', state.isLogged);
     },
     logout: state => {
-      state.isLogged = false
+      state.isLogged = false;
+      storage.set('isLogged', state.isLogged);
+    },
+    setUserId: (state, action) => {
+      state.userId = action.payload;
+      storage.set('userId', state.userId);
+    },
+  },
+});
 
-      storage.set('isLogged', state.isLogged)
-    }
-  }
-})
+export const {login, logout, setUserId} = slice.actions;
 
-export const { login, logout } = slice.actions
-
-export default slice.reducer
+export default slice.reducer;
