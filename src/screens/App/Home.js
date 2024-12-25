666f6e3e00de9a5d31832791c914cbb50bb634ca
umutcanbas <umutbas57@gmail.com>
2024-12-25 20:11:11 +0300

    Add user data management in Home screen; dispatch userId to Redux and save user info in Firebase

--- src/screens/App/Home.js
+++ src/screens/App/Home.js
@@ -1,11 +1,43 @@
 import {SafeAreaView, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import TopMenu from '../../components/TopMenu';
 
+import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+
+import {useDispatch} from 'react-redux';
+import {setUserId} from '../../redux/slice';
+
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const currentUser = auth().currentUser;
+
+    dispatch(setUserId(currentUser.uid));
+    if (currentUser) {
+      const userInfo = {
+        userId: currentUser.uid,
+        username: 'Guest',
+        userAddress: 'Not Provided',
+      };
+
+      database()
+        .ref(`/users/${currentUser.uid}`)
+        .set(userInfo)
+        .then(() => {
+          console.log('User data saved successfully to Firebase.');
+        })
+        .catch(error => {
+          console.error('Error saving user data:', error);
+        });
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <TopMenu title='Second Cycyle' />
+      <TopMenu title="Second Cycyle" />
       <Text>Home</Text>
     </SafeAreaView>
   );
@@ -16,6 +48,6 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'white'
+    backgroundColor: 'white',
   },
 });
