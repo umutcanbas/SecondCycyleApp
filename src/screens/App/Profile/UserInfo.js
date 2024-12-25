3d30127e0dd94548bff289345cda7d855060571b
umutcanbas <umutbas57@gmail.com>
2024-12-25 20:11:27 +0300

    Implement user information input and saving functionality in UserInfo screen; integrate Firebase for data storage

--- src/screens/App/Profile/UserInfo.js
+++ src/screens/App/Profile/UserInfo.js
@@ -1,14 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-const UserInfo = () => {
+import TopMenu from '../../../components/TopMenu';
+import Input from '../../../components/Input';
+import Button from '../../../components/Button';
+
+import {useSelector} from 'react-redux';
+
+import database from '@react-native-firebase/database';
+
+const UserInfo = ({navigation}) => {
+  const [userName, setUserName] = React.useState('');
+  const [userAddress, setUserAddress] = React.useState('');
+
+  const userId = useSelector(state => state.slice.userId);
+
+  const saveInfo = () => {
+    const userInfo = {
+      userId,
+      userName,
+      userAddress,
+    };
+
+    database()
+      .ref(`/users/${userId}`)
+      .set(userInfo)
+      .then(() => {
+        console.log('User data saved successfully to Firebase.');
+      })
+      .catch(error => {
+        console.error('Error saving user data:', error);
+      });
+
+    navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>UserInfo</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <TopMenu
+        title="User Info"
+        onPressLeft={() => navigation.goBack()}
+        leftIcon="back"
+      />
 
-export default UserInfo
+      <Input
+        value={userName}
+        onChangeText={setUserName}
+        placeholder="Enter your username"
+      />
+      <Input
+        value={userAddress}
+        onChangeText={setUserAddress}
+        placeholder="Enter your address"
+      />
 
-const styles = StyleSheet.create({})
+      <Button
+        title="Save"
+        onPress={saveInfo}
+        containerStyles={{backgroundColor: 'red'}}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default UserInfo;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
