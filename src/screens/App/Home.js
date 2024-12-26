670131ec565db79489d5420c1dded5e1775027e2
umutcanbas <umutbas57@gmail.com>
2024-12-26 15:17:14 +0300

    Remove Firebase user data initialization logic from Home screen

--- src/screens/App/Home.js
+++ src/screens/App/Home.js
@@ -1,48 +1,9 @@
 import {SafeAreaView, Text, StyleSheet} from 'react-native';
-import React, {useEffect} from 'react';
+import React from 'react';
 
 import TopMenu from '../../components/TopMenu';
 
-import auth from '@react-native-firebase/auth';
-import database from '@react-native-firebase/database';
-
 const Home = () => {
-  useEffect(() => {
-    const currentUser = auth().currentUser;
-
-    if (currentUser) {
-      database()
-        .ref(`/users/${currentUser.uid}`)
-        .once('value')
-        .then(snapshot => {
-          if (!snapshot.exists()) {
-            const userInfo = {
-              userId: currentUser.uid,
-              username: 'Guest',
-              userAddress: 'Not Provided',
-            };
-
-            database()
-              .ref(`/users/${currentUser.uid}`)
-              .set(userInfo)
-              .then(() => {
-                console.log('User data saved successfully to Firebase.');
-              })
-              .catch(error => {
-                console.error('Error saving user data:', error);
-              });
-          } else {
-            console.log('User data already exists in Firebase.');
-          }
-        })
-        .catch(error => {
-          console.error('Error checking user data:', error);
-        });
-    } else {
-      console.error('No authenticated user found.');
-    }
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
       <TopMenu title="Second Cycle" />
