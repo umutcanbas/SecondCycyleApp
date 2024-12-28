e85917ef12e6b14f9a13f507768ae7da44749a84
umutcanbas <umutbas57@gmail.com>
2024-12-28 18:26:07 +0300

    Add Products component and enhance TopMenu with navigation and icon

--- src/screens/App/Home/Home.js
+++ src/screens/App/Home/Home.js
@@ -1,21 +1,22 @@
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, Text, StyleSheet, View} from 'react-native';
 import React from 'react';
 
 import TopMenu from '../../../components/TopMenu';
 import Button from '../../../components/Button';
 
 import routes from '../../../navigation/routes';
+import Products from '../../../components/Products';
 
 const Home = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
-      <TopMenu title="Second Cycle" />
-
-      <Button
-        title="Add Product"
-        onPress={() => navigation.navigate(routes.ADD_PRODUCT)}
-        containerStyles={{backgroundColor: 'red'}}
+      <TopMenu
+        title="Second Cycle"
+        onPressRight={() => navigation.navigate(routes.ADD_PRODUCT)}
+        rightIcon="plus"
       />
+
+      <Products />
     </SafeAreaView>
   );
 };
