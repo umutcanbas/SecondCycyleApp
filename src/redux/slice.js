e7411d21451b9cd900c21e078e83b2e3e1a725d5
umutcanbas <umutbas57@gmail.com>
2024-12-29 23:46:20 +0300

    Add favorite list management to Redux slice with actions to change and clear favorites

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -5,8 +5,12 @@ import {MMKV} from 'react-native-mmkv';
 const storage = new MMKV();
 const isLogged = storage.getBoolean('isLogged');
 
+const favoriteListStorage = storage.getString('favoriteList');
+const favoriteList = favoriteListStorage && JSON.parse(favoriteListStorage);
+
 const initialState = {
   isLogged: isLogged || false,
+  favoriteList: favoriteList || [],
 };
 
 const slice = createSlice({
@@ -21,9 +25,29 @@ const slice = createSlice({
       state.isLogged = false;
       storage.set('isLogged', state.isLogged);
     },
+    clearFavorites: state => {
+      state.favoriteList = [];
+      storage.set('favoriteList', JSON.stringify(state.favoriteList));
+    },
+    changeFavoriteList: (state, action) => {
+      const coin = action.payload;
+
+      const isFavorite = state.favoriteList.find(item => item.id === coin.id);
+
+      if (isFavorite) {
+        state.favoriteList = state.favoriteList.filter(
+          item => item.id !== coin.id,
+        );
+      } else {
+        state.favoriteList.push(coin);
+      }
+
+      storage.set('favoriteList', JSON.stringify(state.favoriteList));
+    },
   },
 });
 
-export const {login, logout} = slice.actions;
+export const {login, logout, changeFavoriteList, clearFavorites} =
+  slice.actions;
 
 export default slice.reducer;
